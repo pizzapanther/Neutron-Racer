fa8e7fd70ad7e1b8824c4ce8ecb2d6994519391c
Paul Bailey <paul.m.bailey@gmail.com>
2014-07-13 00:17:45 +0000

    good whack at the race algorithm

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 
-var RaceApp = angular.module('race', ['ui.bootstrap']);
+var RaceApp = angular.module('race', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+
+RaceApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {controller:'WelcomeCtrl', templateUrl:'welcome.html'})
+    .when('/new-race', {controller:'NewRaceCtrl', templateUrl:'new-race.html'})
+    .when('/start-race', {controller:'RaceCtrl', templateUrl:'race.html'})
+    .otherwise({redirectTo: '/'});
+});
 
 RaceApp.run(function ($rootScope, $modal) {
   $rootScope.manifest = chrome.runtime.getManifest();
+  
+  $rootScope.reset = function () {
+    $rootScope.new_racers = [];
+    $rootScope.new_tracks = 2;
+    $rootScope.load_race = '';
+  };
+  
+  $rootScope.reset();
 });
