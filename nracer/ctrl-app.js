0715c03eefb4eb9fef85c1a8eb1464dcb17776d0
Paul Bailey <paul.m.bailey@gmail.com>
2015-10-24 23:43:11 +0000

    more responsive header

--- nracer/ctrl-app.js
+++ nracer/ctrl-app.js
@@ -1,5 +1,5 @@
 nracer.controller('AppCtrl', function ($scope, $location) {
-  $scope.version = '15.10.1';
+  $scope.version = '15.10.2';
   
   $scope.goto = function (url) {
     $location.url(url);
