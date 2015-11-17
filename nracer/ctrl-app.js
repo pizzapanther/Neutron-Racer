afc1e1cb888e4573d1ff00d797d2ec14133a2134
Paul Bailey <paul.m.bailey@gmail.com>
2015-11-17 16:17:48 +0000

    updated version

--- nracer/ctrl-app.js
+++ nracer/ctrl-app.js
@@ -1,5 +1,5 @@
 nracer.controller('AppCtrl', function ($scope, $location) {
-  $scope.version = '15.10.2';
+  $scope.version = '15.10.3';
   
   $scope.goto = function (url) {
     $location.url(url);
